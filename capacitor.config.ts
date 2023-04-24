620c8a17f993df83f98f4fc5d843c23d47a33b7d
ANDERSON DE ARAUJO ROCHA <04898487459@sesed.interno>
2023-04-24 09:52:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todas_funcoes_ionic7',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
