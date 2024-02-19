3f476a61300f52c71c2b6fdba45e32fff5e5f9c2
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2024-02-19 11:05:01 -0300

    feito o delete imagem da camera

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../../services/photo.service';
+import { PhotoService, UserPhoto } from '../../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { PhotoService } from '../../services/photo.service';
 })
 export class CameraPage implements OnInit {
   
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
   
 
   async ngOnInit() {
@@ -18,6 +19,28 @@ export class CameraPage implements OnInit {
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
