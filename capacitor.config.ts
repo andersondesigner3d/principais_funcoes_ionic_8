872b7e41a95d99492adbc2cc28723bad4ebe268d
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2024-05-16 08:32:40 -0300

    corrigido bugs nos dados do google play

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.natalgamestudio.todas_funcoes_ionic_8',
-  appName: 'todas_funcoes_ionic_8',
+  appId: 'io.natalgamestudio.principais_funcoes_ionic_8',
+  appName: 'Principais_funcoes_ionic_8',
   webDir: 'www',
   bundledWebRuntime: false
 };
