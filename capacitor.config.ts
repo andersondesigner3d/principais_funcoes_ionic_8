f50fe8ce475b7c6715954000ec4873783d05cc69
Anderson Rocha <32132934+andersondesigner3d@users.noreply.github.com>
2024-07-12 15:26:07 -0300

    adicionado icon e splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,25 @@ const config: CapacitorConfig = {
   appId: 'io.natalgamestudio.principais_funcoes_ionic_8',
   appName: 'Principais_funcoes_ionic_8',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
